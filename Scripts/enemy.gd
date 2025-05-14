a715085075f3284624c3cd3bff9a1b380baa30e1
rnseo007 <rnseo007@outlook.kr>
2025-05-14 23:49:46 +0900

    적 이동, 적 애니메이션 제어(x값 10 이상 시 애니메이션 변경), 플레이 화면 HUD 임시 제작

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,13 +2,20 @@ extends CharacterBody2D
 
 @export var speed : int = 100
 
-@onready var playerPos : Vector2 = $"../Player".global_position
-
-var pos = $".".global_position
-
-func _process(delta: float) -> void:
+func _process(_delta):
 	velocity = Vector2.ZERO
 	
-	var direction = Vector2(-1 if playerPos.x < pos.x else 1, -1 if playerPos.y < pos.y else 1)
+	var playerPos : Vector2 = $"../Player".global_position
+	
+	var direction = Vector2(
+		0 if playerPos.x == global_position.x else -1 if playerPos.x < global_position.x else 1,
+		0 if playerPos.y == global_position.y else -1 if playerPos.y < global_position.y else 1
+	)
 	
 	velocity = direction.normalized() * speed
+	
+	var pte
+	if abs(playerPos.x-global_position.x) > 10:
+		pte = direction.x
+		$AnimationTree.set("parameters/Walk/blend_position", pte)
+	move_and_slide()
