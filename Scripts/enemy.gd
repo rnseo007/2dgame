62112bd56e835265faf7a1534f7d2edda2b6b532
rnseo007 <rnseo007@outlook.kr>
2025-05-21 22:20:17 +0900

    적 AI, 애니메이션 수정 완. HUD 수정.

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,42 +3,35 @@ extends CharacterBody2D
 class_name Entity
 
 @export var speed : float = 100
-@export var cur_hp : float = 50
-@onready var hit_anim_player = $HitAnimationPlayer
+@export var cur_hp : int = 50
+
+@onready var sprite = $Enemy_sprite2D
+@onready var walk_anim = $WalkAnimationPlayer
+@onready var hit_anim = $HitAnimationPlayer
 @onready var player = get_tree().get_first_node_in_group("player")
 
-signal spawned(entity : Node2D)
-signal dead(entity : Node2D)
-
-func _ready() -> void:
-	spawned.emit($".")
-	print("spawned")
+func _ready():
+	walk_anim.play("walk")
 
 func death():
-	dead.emit($".")
 	queue_free()
 
 func _process(_delta):
-	velocity = Vector2.ZERO
+	var direction = global_position.direction_to(player.global_position)
 	
-	var playerPos : Vector2 = player.global_position
+	velocity = direction * speed
 	
-	var direction = Vector2(
-		0 if playerPos.x == global_position.x else -1 if playerPos.x < global_position.x else 1,
-		0 if playerPos.y == global_position.y else -1 if playerPos.y < global_position.y else 1
-	)
-	
-	velocity = direction.normalized() * speed
-
-	if abs(playerPos.x-global_position.x) > 10:
-		$AnimationTree.set("parameters/Walk/blend_position", direction.x)
+	if direction.x > 0.1:
+		sprite.flip_h = false
+	elif direction.x < -0.1:
+		sprite.flip_h = true
 	
 	move_and_slide()
 	$TestText.text = "<ORC>\n{0}\nHP: {1}\nSPEED: {2}".format({0:global_position.floor(),1:cur_hp,2:speed})
 
 func take_damage(damage):
 	cur_hp -= damage
-	hit_anim_player.play("Hit_Animation")
+	hit_anim.play("Hit_Animation")
 	
 	if cur_hp <= 0:
 		death()
