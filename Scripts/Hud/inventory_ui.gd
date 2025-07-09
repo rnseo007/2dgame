f482d08d6458e869fcf132f2d545c6b2fea91b32
rnseo007 <rnseo007@outlook.kr>
2025-07-09 23:57:52 +0900

    인벤 씬 제작중

--- Scripts/Hud/inventory_ui.gd
+++ Scripts/Hud/inventory_ui.gd
@@ -2,8 +2,10 @@ extends Control
 
 @onready var card = preload("res://Scenes/HUD_Scenes/inv_card.tscn")
 @onready var gird_container = $ScrollContainer/MarginContainer/GridContainer
+@onready var text_display = $card_text
 
 func _ready() -> void:
 	for i in range(0, 23):
 		var new_card = card.instantiate()
 		gird_container.call_deferred("add_child", new_card)
+	text_display.text = "CARD : %d" % [23]
