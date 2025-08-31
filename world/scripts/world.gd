3a1f06832b73d70a0fa5ea34eff98527678e5c8d
rnseo007 <rnseo007@outlook.kr>
2025-08-31 22:33:58 +0900

    프로젝트 망

--- world/scripts/world.gd
+++ world/scripts/world.gd
@@ -5,8 +5,8 @@ var gen = WorldGen.new()
 var tile_placer = TilePlacer.new()
 
 func _ready() -> void:
-	#var test := bsp.generate()
+	var test := bsp.generate()
 	#var seed = gen.seed_set()
-	var test = gen.generate_room(null)
+	#var test = gen.generate_room(null)
 	#print(test)
 	tile_placer.print_to_tilemap(test, $FLOOR, $WALL)
